98328d922ecec33cd77844c66847ee2f44643077
Vikram Rangarajan <vikram.rangaraja@gmail.com>
2026-01-13 19:16:54 -0500

    Added config

---
+++ lua/plugins/rainbow.lua
@@ -0,0 +1,11 @@
+return {
+    "HiPhish/rainbow-delimiters.nvim",
+    config = function()
+        local rainbow_delimiters = require("rainbow-delimiters")
+        require("rainbow-delimiters.setup").setup({
+            strategy = {
+                [''] = rainbow_delimiters.strategy['global'],
+            },
+        })
+    end
+}
