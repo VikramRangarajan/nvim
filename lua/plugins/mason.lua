78bab72414be79d4533c2b07253bb6f868861c34
Vikram Rangarajan <vikram.rangaraja@gmail.com>
2026-01-13 19:22:26 -0500

    Formatted everything

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,24 +1,24 @@
 return {
-    "williamboman/mason.nvim",
-    dependencies = {
-        "williamboman/mason-lspconfig.nvim",
-    },
-    config = function()
-        -- import mason
-        local mason = require("mason")
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+	},
+	config = function()
+		-- import mason
+		local mason = require("mason")
 
-        -- import mason-lspconfig
-        local mason_lspconfig = require("mason-lspconfig")
+		-- import mason-lspconfig
+		local mason_lspconfig = require("mason-lspconfig")
 
-        -- enable mason and configure icons
-        mason.setup()
+		-- enable mason and configure icons
+		mason.setup()
 
-        mason_lspconfig.setup({
-            ensure_installed = {
-                "ty",
-                "rust_analyzer",
-                "lua_ls"
-            }
-        })
-    end,
+		mason_lspconfig.setup({
+			ensure_installed = {
+				"ty",
+				"rust_analyzer",
+				"lua_ls",
+			},
+		})
+	end,
 }
