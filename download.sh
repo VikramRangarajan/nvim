100e055e586e6489be7260831684ce21d26a9952
Vikram Rangarajan <rangarav@purdue.edu>
2026-02-25 16:50:19 -0500

    Added uv installation

--- download.sh
+++ download.sh
@@ -1,3 +1,4 @@
+curl -LsSf https://astral.sh/uv/install.sh | sh
 mkdir -p lazygit_stuff ~/.local/bin
 wget https://github.com/neovim/neovim/releases/download/nightly/nvim-linux-x86_64.appimage -O ~/.local/bin/nvim # should be neovim 0.12
 chmod u+x ~/.local/bin/nvim
@@ -5,3 +6,4 @@ wget https://github.com/jesseduffield/lazygit/releases/download/v0.59.0/lazygit_
 tar -xvzf lazygit_0.59.0_linux_x86_64.tar.gz -C lazygit_stuff
 mv lazygit_stuff/lazygit ~/.local/bin
 rm -r lazygit*
+uv tool install ruff
