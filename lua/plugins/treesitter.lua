d7b90869c1149ba57f3f58e2f9cb4427a96cf511
Vikram Rangarajan <vikram.rangaraja@gmail.com>
2026-03-12 16:12:13 -0400

    Added folding and image.nvim

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -12,6 +12,7 @@ return {
 				"vimdoc",
 				"c",
 				"query",
+				"python",
 			},
 			sync_install = false,
 			auto_install = true,
@@ -21,8 +22,17 @@ return {
 			},
 			indent = {
 				enable = true,
-				disable = { "python", "c" }, -- these and some other langs don't work well
+				-- disable = { "python", "c" }, -- these and some other langs don't work well
 			},
+		})
+		vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+		vim.wo[0][0].foldmethod = "expr"
+		vim.opt.foldlevelstart = 99
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "python",
+			callback = function()
+				vim.opt_local.foldmethod = "indent"
+			end,
 		})
 	end,
 }
